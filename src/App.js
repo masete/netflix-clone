8e81e67a8f38976405a800e04acf3af383de25d7
masete <nicholasmasete72@gmail.com>
2022-08-04 12:13:00 +0300

    feat: added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
   </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className='flex items-center justify-between pr-4 z-[100] w-full absolute'>
         <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
         <div>
-            <button>Sign In</button>
-            <button>Sign Up</button>
+            <button className='text-white pr-4 '>Sign In</button>
+            <button className='text-white px-6 bg-red-600 rounded py-2'>Sign Up</button>
         </div>
     </div>
   )
