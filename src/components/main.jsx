cb474cf10c4e6f436bd061aadd2263dd8840b8df
masete <nicholasmasete72@gmail.com>
2022-08-04 17:58:25 +0300

    css: added tailwind styling to ain.js

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -16,7 +16,16 @@ const Main = () => {
   return (
     <div className='w-full h-[550px] text-white'>
         <div className='w-full h-full'>
-            <img className='w-full h-full' src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`} alt={movie?.title}/>
+            <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+            <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`} alt={movie?.title}/>
+            <div className='absolute w-full top-[20%] p-4 md:p-8'>
+                <h1 className='text-3xl md:text-5xl'>{movie?.title}</h1>
+                <div>
+                    <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                    <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+                </div>
+            </div>
+            
         </div>
     </div>
   )
