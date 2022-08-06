d9c78b4013b9eea4edf84a059d3beec1d49e87d2
masete <nicholasmasete72@gmail.com>
2022-08-06 09:57:42 +0300

    chore: created auth contexct for authentication

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
+import { AuthContextProvider} from './context/AuthContext'
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>} />
       </Routes>
+    </AuthContextProvider>
+      
   </>
   );
 }
