02e49edb43c5d26c45020a0b8988f35dfe17b196
masete <nicholasmasete72@gmail.com>
2022-08-06 08:08:01 +0300

    chore: added firebase configs

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVCKLDcHJAZshOo-L06eyX7ze5WTuF6mI",
+  authDomain: "netflix-react-yt-4ce23.firebaseapp.com",
+  projectId: "netflix-react-yt-4ce23",
+  storageBucket: "netflix-react-yt-4ce23.appspot.com",
+  messagingSenderId: "458779464818",
+  appId: "1:458779464818:web:ffe24cd85555515464deeb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
