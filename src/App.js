5052a2c7e941794f6f3a475d8c68165de1e7eb9e
masete <nicholasmasete72@gmail.com>
2022-08-10 08:46:02 +0300

    feat: added saved shows

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
 import Home from './pages/Home';
-import { AuthContextProvider} from './context/AuthContext'
 import Login from './pages/Login';
-import SignUp from './pages/Signup';
+import Signup from './pages/Signup';
 import Account from './pages/Account';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <>
-    <AuthContextProvider>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<SignUp/>} />
-        <Route path='/account' element={
-          <ProtectedRoute>
-            <Account/>
-          </ProtectedRoute>
-        }/>
-      </Routes>
-    </AuthContextProvider>
-      
-  </>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
